533f7e16d4bd6610466352427a5b72b39d961b8e
KYAN <tvquan12345@gmail.com>
2025-12-02 00:47:02 +1000

    Main Menu

---
+++ Views/MainMenuWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿//using Morskoy_Goy.Services;
+using System.Windows;
+using System.Windows.Navigation;
+
+namespace Morskoy_Goy.Views
+{
+    public partial class MainMenuWindow : Window
+    {
+        private readonly NavigationService _navigationService;
+
+        public MainMenuWindow()
+        {
+            InitializeComponent();
+            //_navigationService = new NavigationService();
+        }
+
+        private void OnStartGameClick(object sender, RoutedEventArgs e)
+        {
+            //var lobbyWindow = new LobbyWindow();
+            //lobbyWindow.Show();
+            this.Close();
+        }
+
+        private void OnExitClick(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void OnMouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+    }
+}
