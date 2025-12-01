8206fa1c063356d15e0f6cb0c55b4de9f9d3a885
KYAN <tvquan12345@gmail.com>
2025-12-02 00:06:04 +1000

    Adding Server and Gameroom

---
+++ Network/Server/ConnectedClient.cs
@@ -0,0 +1,151 @@
+﻿using Morskoy_Goy.Network.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Morskoy_Goy.Network.Server
+{
+    public class ConnectedClient
+    {
+        private TcpClient _tcpClient;
+        private NetworkStream _stream;
+        private GameServer _server;
+        public string ClientId { get; } = Guid.NewGuid().ToString();
+        public string PlayerName { get; set; }
+        public GameRoom CurrentRoom { get; set; }
+
+        public ConnectedClient(TcpClient client, GameServer server)
+        {
+            _tcpClient = client;
+            _stream = client.GetStream();
+            _server = server;
+        }
+
+        public async Task StartListeningAsync()
+        {
+            try
+            {
+                var buffer = new byte[4096];
+
+                while (_tcpClient.Connected)
+                {
+                    var bytesRead = await _stream.ReadAsync(buffer, 0, buffer.Length);
+                    if (bytesRead == 0) break;
+
+                    var json = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    var message = JsonSerializer.Deserialize<NetworkMessage>(json);
+
+                    await ProcessMessageAsync(message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка клиента {ClientId}: {ex.Message}");
+            }
+            finally
+            {
+                _server.RemoveClient(this);
+            }
+        }
+
+        private async Task ProcessMessageAsync(NetworkMessage message)
+        {
+            switch (message.Type)
+            {
+                case MessageType.Connect:
+                    PlayerName = (message.Data as JsonElement?)?.GetProperty("playerName").GetString();
+                    await SendRoomList();
+                    break;
+
+                case MessageType.CreateRoom:
+                    var createRequest = JsonSerializer.Deserialize<CreateRoomRequest>(
+                        (message.Data as JsonElement?)?.ToString() ?? "{}");
+
+                    var room = _server.CreateRoom(
+                        createRequest.RoomName,
+                        createRequest.PlayerName,
+                        createRequest.MaxPlayers);
+
+                    CurrentRoom = room;
+                    room.AddPlayer(this, isHost: true);
+
+                    await SendRoomInfo(room);
+                    break;
+
+                case MessageType.JoinRoom:
+                    var joinRequest = JsonSerializer.Deserialize<JoinRoomRequest>(
+                        (message.Data as JsonElement?)?.ToString() ?? "{}");
+
+                    var targetRoom = _server.FindRoom(joinRequest.RoomId);
+                    if (targetRoom != null && targetRoom.CanJoin())
+                    {
+                        CurrentRoom = targetRoom;
+                        targetRoom.AddPlayer(this, isHost: false);
+
+                        await SendRoomInfo(targetRoom);
+                        await targetRoom.BroadcastPlayerList();
+                    }
+                    else
+                    {
+                        await SendError("Не удалось присоединиться к комнате");
+                    }
+                    break;
+
+                case MessageType.LeaveRoom:
+                    CurrentRoom?.RemovePlayer(this);
+                    CurrentRoom = null;
+                    await SendRoomList();
+                    break;
+
+                case MessageType.StartGame:
+                    CurrentRoom?.StartGame();
+                    break;
+            }
+        }
+
+        public async Task SendMessage(NetworkMessage message)
+        {
+            var json = JsonSerializer.Serialize(message);
+            var data = Encoding.UTF8.GetBytes(json);
+            await _stream.WriteAsync(data, 0, data.Length);
+        }
+
+        private async Task SendRoomList()
+        {
+            var rooms = _server.GetRoomList();
+            await SendMessage(new NetworkMessage
+            {
+                Type = MessageType.RoomList,
+                Data = rooms
+            });
+        }
+
+        private async Task SendRoomInfo(GameRoom room)
+        {
+            await SendMessage(new NetworkMessage
+            {
+                Type = MessageType.RoomList,
+                Data = new { room = room.GetInfo(), players = room.GetPlayerList() }
+            });
+        }
+
+        private async Task SendError(string error)
+        {
+            await SendMessage(new NetworkMessage
+            {
+                Type = MessageType.Error,
+                Data = new { message = error }
+            });
+        }
+
+        public void Disconnect()
+        {
+            _stream?.Close();
+            _tcpClient?.Close();
+        }
+    }
+}
---
+++ Network/Server/GameRoom.cs
@@ -0,0 +1,118 @@
+﻿using Morskoy_Goy.Network.Common;
+using Morskoy_Goy.Network.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Morskoy_Goy.Network.Server
+{
+    public class GameRoom
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public string Name { get; }
+        public int MaxPlayers { get; }
+        public bool IsGameStarted { get; private set; }
+
+        private Dictionary<string, ConnectedClient> _players = new();
+        private ConnectedClient _host;
+
+        public int PlayerCount => _players.Count;
+        public string HostName => _host?.PlayerName ?? "Нет хоста";
+
+        public GameRoom(string name, int maxPlayers)
+        {
+            Name = name;
+            MaxPlayers = maxPlayers;
+        }
+
+        public bool CanJoin()
+        {
+            return !IsGameStarted && _players.Count < MaxPlayers;
+        }
+
+        public void AddPlayer(ConnectedClient client, bool isHost)
+        {
+            _players[client.ClientId] = client;
+
+            if (isHost)
+            {
+                _host = client;
+            }
+
+            client.CurrentRoom = this;
+        }
+
+        public void RemovePlayer(ConnectedClient client)
+        {
+            _players.Remove(client.ClientId);
+
+            if (client == _host && _players.Any())
+            {
+                _host = _players.First().Value;
+            }
+
+            BroadcastPlayerList();
+
+            if (_players.Count == 0)
+            {
+                // Удалить комнату из сервера (потом добавить логику пока леньььььььь)
+            }
+        }
+
+        public void StartGame()
+        {
+            IsGameStarted = true;
+
+            var startMessage = new NetworkMessage
+            {
+                Type = MessageType.GameStart,
+                Data = new { roomId = Id }
+            };
+
+            BroadcastMessage(startMessage);
+        }
+
+        public async Task BroadcastMessage(NetworkMessage message)
+        {
+            var tasks = _players.Values.Select(client => client.SendMessage(message));
+            await Task.WhenAll(tasks);
+        }
+
+        public async Task BroadcastPlayerList()
+        {
+            var playerList = GetPlayerList();
+            var message = new NetworkMessage
+            {
+                Type = MessageType.PlayerList,
+                Data = playerList
+            };
+
+            await BroadcastMessage(message);
+        }
+
+        public List<PlayerInfo> GetPlayerList()
+        {
+            return _players.Values.Select(client => new PlayerInfo
+            {
+                Id = client.ClientId,
+                Name = client.PlayerName,
+                IsHost = client == _host,
+                IsReady = false // Здесь тоже надо потом  добавить логику готовности пока хзхзхзхз
+            }).ToList();
+        }
+
+        public RoomInfo GetInfo()
+        {
+            return new RoomInfo
+            {
+                Id = Id,
+                Name = Name,
+                CurrentPlayers = PlayerCount,
+                MaxPlayers = MaxPlayers,
+                HostName = HostName,
+                InGame = IsGameStarted
+            };
+        }
+    }
+}
